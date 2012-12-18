3625e54c11c0577229164f81b4345773a9af66c4
Bradly Alicea <freejumper@yahoo.com>
2012-12-18 10:39:20 -0500

    Jensen-Shannon Divergence (MATLAB code), v2.0
    Addition of code for interval normalization.

--- JSD.m
+++ JSD.m
@@ -3,6 +3,12 @@
 A1 = data(variable,:);
 X1 = A1/max(A1);
 % normalize all values in emsemble to maximum value (1.0). NOTE: this is not a formal probability.
+% Alternative method: normalize by range (interval normalization) 
+% 0.0 = minimum value, 1.0 = maximal value.
+interval-whole = MAX(A)-MIN(A);
+interval = MAX(A)-A(xi);
+An = interval/interval-whole;
+%%
 X2 = log10(X1);
 % for customized (base n) logarithm, use the following line: X2 = (log10(X1)/log10(n)).
 c3 = -(sum(X1)*sum(X2));
